30122fed5e8789a300d3bf67b19d9aeaaf037af3
thebenius <bennyhaag@protonmail.com>
2020-08-30 20:04:57 +0200

    refactored code (OOP)

--- js/ddupload.js
+++ js/ddupload.js
@@ -1,6 +1,4 @@
 
-
-
 /**
  * @params {array} files List of file items
  * @return FileList
@@ -12,84 +10,102 @@ function FileListItems (files) {
 }
 
 
-function addThumbs(dropzone, files) {
 
-    // make prompt smaller
-    if( files.length > 0 ) {
-        $($(dropzone).find("span")[0]).addClass("small_prompt");
-    }
-    
+function addThumb(dropzone, file) {
 
-    $.each(files, function(index, file) {
+    /**
+     *  ████████╗██╗  ██╗██╗   ██╗███╗   ███╗██████╗ ███╗  ██╗ █████╗ ██╗██╗     
+     *  ╚══██╔══╝██║  ██║██║   ██║████╗ ████║██╔══██╗████╗ ██║██╔══██╗██║██║     
+     *     ██║   ███████║██║   ██║██╔████╔██║██████╦╝██╔██╗██║███████║██║██║     
+     *     ██║   ██╔══██║██║   ██║██║╚██╔╝██║██╔══██╗██║╚████║██╔══██║██║██║     
+     *     ██║   ██║  ██║╚██████╔╝██║ ╚═╝ ██║██████╦╝██║ ╚███║██║  ██║██║███████╗
+     *     ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚═╝  ╚══╝╚═╝  ╚═╝╚═╝╚══════╝
+     */
 
-        let thumbnail = $("<div></div>");
-        thumbnail.addClass("thumb");
-        $(dropzone).append(thumbnail);
-        $(thumbnail).get(0).dataset.label = file.name;
+    // create Thumbnail
+    let thumbnail = $("<div></div>").get(0);
+    $(thumbnail).addClass("thumb");
+    $(dropzone).append(thumbnail);
+    thumbnail.dataset.label = file.name;
 
 
-        // click on item to delete it
-        $(thumbnail).click(function(){
-            let index = Array.from($(dropzone).find(".thumb")).indexOf(this);
-            delFile(this, index);
-        });
 
-        // show thumbnai
-        if ( file.type.startsWith("image/") ) {
-            let reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = function() {
-                $(thumbnail).css("background-image", `url('${reader.result}')`);
-            };
+    /**
+     *  ███╗   ███╗███████╗████████╗██╗  ██╗ █████╗ ██████╗  ██████╗
+     *  ████╗ ████║██╔════╝╚══██╔══╝██║  ██║██╔══██╗██╔══██╗██╔════╝
+     *  ██╔████╔██║█████╗     ██║   ███████║██║  ██║██║  ██║╚█████╗ 
+     *  ██║╚██╔╝██║██╔══╝     ██║   ██╔══██║██║  ██║██║  ██║ ╚═══██╗
+     *  ██║ ╚═╝ ██║███████╗   ██║   ██║  ██║╚█████╔╝██████╔╝██████╔╝
+     *  ╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚════╝ ╚═════╝ ╚═════╝ 
+     */
+
+
+    /**
+     * DELETE THUMBNAIL AND CORRESPONDING FILE FROM DROPZONE INPUT
+     * 
+     * @param {Int} index 
+     */
+    thumbnail.delete = function(index) {
+
+        let dropzone = this.closest(".dropzone");
+
+        let input = $(dropzone).find("input")[0];
+        let list = Array.from(input.files);
+        
+        list.splice(index, 1);
+
+        input.files = new FileListItems(list);
+
+        // reset prompt if list empty
+        if (list.length == 0) {
+            $($(dropzone).find("span")[0]).removeClass("small_prompt");
         }
 
-    });
-}
-
-
-/**
- * Adds fileList to dropzone
- * 
- * @param {HTMLElement} dropzone 
- * @param {FileList} files 
- */
-function addFiles(dropzone, files) {
-
-    let input = $(dropzone).find("input")[0];
-
-    let newList = Array.from(files);
-    let oldList = Array.from(input.files);
-    let list = newList.concat(oldList);
-    input.files = new FileListItems(list);
-
-    addThumbs(dropzone, files);
-}
-
-
-/**
- * delete file from dropzone
- * 
- * @param {HTMLElement} dropzone 
- * @param {Int} index 
- */
-function delFile(item, index) {
-
-    let dropzone = item.closest(".dropzone");
-
-    let input = $(dropzone).find("input")[0];
-    let list = Array.from(input.files);
-    
-    list.splice(index, 1);
-
-    input.files = new FileListItems(list);
-
-    // reset prompt if list empty
-    if (list.length == 0) {
-        $($(dropzone).find("span")[0]).removeClass("small_prompt");
+        $(this).remove();
     }
 
-    $(item).remove();
+
+
+    /**
+     *  ██╗     ██╗ ██████╗████████╗███████╗███╗  ██╗███████╗██████╗ 
+     *  ██║     ██║██╔════╝╚══██╔══╝██╔════╝████╗ ██║██╔════╝██╔══██╗
+     *  ██║     ██║╚█████╗    ██║   █████╗  ██╔██╗██║█████╗  ██████╔╝
+     *  ██║     ██║ ╚═══██╗   ██║   ██╔══╝  ██║╚████║██╔══╝  ██╔══██╗
+     *  ███████╗██║██████╔╝   ██║   ███████╗██║ ╚███║███████╗██║  ██║
+     *  ╚══════╝╚═╝╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚══╝╚══════╝╚═╝  ╚═╝
+     */
+
+
+    // add click listener to thumbnail
+    $(thumbnail).click(function(){
+        let index = Array.from($(dropzone).find(".thumb")).indexOf(this);
+        this.delete(index);
+    });
+
+
+
+
+
+
+
+    // make prompt smaller
+    $($(dropzone).find("span")[0]).addClass("small_prompt");
+    
+
+    // show thumbnail if file is image
+    if ( file.type.startsWith("image/") ) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function() {
+            $(thumbnail).css("background-image", `url('${reader.result}')`);
+        };
+    }
+
 }
+
+
+
+
 
 
 
@@ -98,47 +114,147 @@ $(document).ready(function() {
 
 
 
-        // store elements
-        const dropzone = this;
-        const input = $(this).find("input")[0];
-
-        // reload input
-        addThumbs(dropzone, input.files);
 
 
-        $($(this).find("span")[0]).click(function() {
-            let dialog = $("<input></input>");
-            dialog.attr("type","file");
-            dialog.attr("multiple","true");
-            dialog.trigger("click");
-            dialog.change(function() {
-                if (this.files.length > 0) {
-                    addFiles(dropzone, this.files);
-                }
-            });
-        });
+/**
+ *  ██████╗ ██████╗  █████╗ ██████╗ ███████╗ █████╗ ███╗  ██╗███████╗
+ *  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚════██║██╔══██╗████╗ ██║██╔════╝
+ *  ██║  ██║██████╔╝██║  ██║██████╔╝  ███╔═╝██║  ██║██╔██╗██║█████╗  
+ *  ██║  ██║██╔══██╗██║  ██║██╔═══╝ ██╔══╝  ██║  ██║██║╚████║██╔══╝  
+ *  ██████╔╝██║  ██║╚█████╔╝██║     ███████╗╚█████╔╝██║ ╚███║███████╗
+ *  ╚═════╝ ╚═╝  ╚═╝ ╚════╝ ╚═╝     ╚══════╝ ╚════╝ ╚═╝  ╚══╝╚══════╝
+ */
 
-        // define dragover event
-        $(dropzone).on("dragover", function(e) {
-            e.preventDefault();
-            $(this).addClass("drag-over");
-        });
 
-        // define dragleave & dragend event
-        $.each(["dragleave", "dragend"], function(index, event) {
-            $(dropzone).on(event, function() {
-                $(this).removeClass("drag-over");
-            });
-        });
+// store dropzone
+const dropzone = this;
 
-        // handle file drop
-        $(dropzone).on("drop", function(e){
-            e.preventDefault();
-            if (e.originalEvent.dataTransfer.files.length) {
-                addFiles(dropzone,e.originalEvent.dataTransfer.files);
+
+/**
+ *   █████╗ ████████╗████████╗██████╗ ██╗██████╗ ██╗   ██╗████████╗███████╗ ██████╗
+ *  ██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██║██╔══██╗██║   ██║╚══██╔══╝██╔════╝██╔════╝
+ *  ███████║   ██║      ██║   ██████╔╝██║██████╦╝██║   ██║   ██║   █████╗  ╚█████╗ 
+ *  ██╔══██║   ██║      ██║   ██╔══██╗██║██╔══██╗██║   ██║   ██║   ██╔══╝   ╚═══██╗
+ *  ██║  ██║   ██║      ██║   ██║  ██║██║██████╦╝╚██████╔╝   ██║   ███████╗██████╔╝
+ *  ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝  ╚═╝╚═╝╚═════╝  ╚═════╝    ╚═╝   ╚══════╝╚═════╝ 
+ */
+
+dropzone.input = $(this).find("input")[0];
+dropzone.addbtn = $(this).find("span")[0]
+
+
+/**
+ *   ██████╗ ███████╗████████╗████████╗███████╗██████╗ 
+ *  ██╔════╝ ██╔════╝╚══██╔══╝╚══██╔══╝██╔════╝██╔══██╗
+ *  ██║  ██╗ █████╗     ██║      ██║   █████╗  ██████╔╝
+ *  ██║  ╚██╗██╔══╝     ██║      ██║   ██╔══╝  ██╔══██╗
+ *  ╚██████╔╝███████╗   ██║      ██║   ███████╗██║  ██║
+ *   ╚═════╝ ╚══════╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝
+ */
+
+
+// returns boolean if dropzone is enabled
+dropzone.isEnabled = function() {
+    return !$(dropzone).hasClass("dropzone-disabled");
+}
+
+
+/**
+ *  ███╗   ███╗███████╗████████╗██╗  ██╗ █████╗ ██████╗  ██████╗
+ *  ████╗ ████║██╔════╝╚══██╔══╝██║  ██║██╔══██╗██╔══██╗██╔════╝
+ *  ██╔████╔██║█████╗     ██║   ███████║██║  ██║██║  ██║╚█████╗ 
+ *  ██║╚██╔╝██║██╔══╝     ██║   ██╔══██║██║  ██║██║  ██║ ╚═══██╗
+ *  ██║ ╚═╝ ██║███████╗   ██║   ██║  ██║╚█████╔╝██████╔╝██████╔╝
+ *  ╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚════╝ ╚═════╝ ╚═════╝ 
+ */
+
+// disable the dropzone
+dropzone.disable = function() {
+    $(dropzone).addClass("dropzone-disabled");
+}
+
+
+/**
+ *  ADDS FILES TO THE DROPZONE INPUT
+ * 
+ * @param {FileList} files 
+ */
+dropzone.addFiles = function(files) {
+
+    let input = $(dropzone).find("input")[0];
+
+    let newList = Array.from(files);
+    let oldList = Array.from(input.files);
+    let list = newList.concat(oldList);
+    input.files = new FileListItems(list);
+
+    $.each(files, function(index, file) {
+        addThumb(dropzone, file);
+    });
+}
+
+
+/**
+ *  ██╗     ██╗ ██████╗████████╗███████╗███╗  ██╗███████╗██████╗ 
+ *  ██║     ██║██╔════╝╚══██╔══╝██╔════╝████╗ ██║██╔════╝██╔══██╗
+ *  ██║     ██║╚█████╗    ██║   █████╗  ██╔██╗██║█████╗  ██████╔╝
+ *  ██║     ██║ ╚═══██╗   ██║   ██╔══╝  ██║╚████║██╔══╝  ██╔══██╗
+ *  ███████╗██║██████╔╝   ██║   ███████╗██║ ╚███║███████╗██║  ██║
+ *  ╚══════╝╚═╝╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚══╝╚══════╝╚═╝  ╚═╝
+ */
+
+// define dragover event
+$(dropzone).on("dragover", function(e) {
+    e.preventDefault();
+    $(this).addClass("drag-over");
+});
+
+// define dragleave & dragend event
+$.each(["dragleave", "dragend"], function(index, event) {
+    $(dropzone).on(event, function() {
+        $(this).removeClass("drag-over");
+    });
+});
+
+// handle file drop
+$(dropzone).on("drop", function(e){
+    e.preventDefault();
+    if (dropzone.isEnabled() && e.originalEvent.dataTransfer.files.length) {
+        dropzone.addFiles(e.originalEvent.dataTransfer.files);
+    }
+    $(this).removeClass("drag-over");
+});
+
+
+$(dropzone.addbtn).click(function() {
+    if (dropzone.isEnabled()) {
+        let dialog = $("<input></input>");
+        dialog.attr("type","file");
+        dialog.attr("multiple","true");
+        dialog.trigger("click");
+        dialog.change(function() {
+            if (this.files.length > 0) {
+                dropzone.addFiles(this.files);
             }
-            $(this).removeClass("drag-over");
         });
+    }
+});
+
+
+
+
+
+
+
+
+// reload input to dropzone
+$.each(dropzone.input.files, function(index, file) {
+    addThumb(dropzone, file);
+});
+
+
+
+
 
 
     });
